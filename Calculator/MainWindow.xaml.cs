3b5b375a8fa6de44290a883404bcf07963231067
Dawson <xmen@uwaterloo.ca>
2021-10-20 10:22:54 -0400

    Refactoring code, no logic changes

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -193,31 +193,4 @@ namespace Calculator
 
         }
     }
-
-    public enum SelectedOperator
-    {
-        Addition,
-        Subtraction,
-        Multiplication,
-        Division
-    }
-    public class SimpleMath
-    {
-        public static double Add(double n1, double n2)
-        {
-            return n1 + n2;
-        }
-        public static double Subtract(double n1, double n2)
-        {
-            return n1 - n2;
-        }
-        public static double Multiply(double n1, double n2)
-        {
-            return n1 * n2;
-        }
-        public static double Divide(double n1, double n2)
-        {
-            return n1/ n2;
-        }
-    }
 }
---
+++ Calculator/SelectedOperator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator
+{
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+}
