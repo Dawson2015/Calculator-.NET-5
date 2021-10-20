d3dee37c3c515ebd091c2faa6d50f96f0185903b
Dawson <xmen@uwaterloo.ca>
2021-10-20 10:52:16 -0400

    Fixed the bug a number is divided by 0. e.g. 6/0

--- Calculator/SimpleMath.cs
+++ Calculator/SimpleMath.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator
+﻿using System.Windows;
+
+namespace Calculator
 {
     public class SimpleMath
     {
@@ -16,6 +18,11 @@
         }
         public static double Divide(double n1, double n2)
         {
+            if (n2 == 0)
+            {
+                MessageBox.Show("Divided by 0 is not supported.","Wrong option",MessageBoxButton.OK,MessageBoxImage.Error);
+                return 0;
+            }
             return n1/ n2;
         }
     }
